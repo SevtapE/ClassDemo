7cb2a7d1f41631a203b91d8fd8b6ddc3f5031b8e
SevtapE <sevtaperbek@outlook.com>
2021-09-13 01:59:18 +0300

    Add project files.

---
+++ CustomerTracker/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerTracker
+{
+    class CustomerManager
+    {
+        List<Customer> _customers;
+        public void Add(Customer customer)
+        {
+
+            Console.WriteLine("{0} is added.",customer.FirstName);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("{0} is deleted.", customer.FirstName);
+        }
+        public List<Customer> List()
+        {
+            return _customers;
+
+        }
+    }
+}
---
+++ CustomerTracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomerTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Josh";
+            customer1.LastName = "Johnson";
+            customer1.PhoneNumber = " 9999999999";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+        }
+    }
+}
